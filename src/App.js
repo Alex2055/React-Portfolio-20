76952c3eed2f0463746953160fa52a9636040db7
Alex2055 <alexfromnashville@gmail.com>
2020-10-15 11:02:13 -0500

    react set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+function App() {
+  
+    return (
+      <div>
+        <main>
+          <div>
+              Hello
+          </div>
+        </main>
+      </div>
+    );
+  }
+  
+  export default App;
