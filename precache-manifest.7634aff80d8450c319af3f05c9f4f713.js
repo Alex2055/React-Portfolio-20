ff42fba6d06fef7b7862986fb09528f9f41cff6d
Alex2055 <alexfromnashville@gmail.com>
2020-10-18 17:48:50 -0500

    Updates

---
+++ precache-manifest.7634aff80d8450c319af3f05c9f4f713.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88289473d85b30d0c271e59acf137a2",
+    "url": "/React-Portfolio-20/index.html"
+  },
+  {
+    "revision": "0f2a371be86de05b6793",
+    "url": "/React-Portfolio-20/static/css/main.cfe12368.chunk.css"
+  },
+  {
+    "revision": "a57a80ab937ae4f0f709",
+    "url": "/React-Portfolio-20/static/js/2.b4251e4f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio-20/static/js/2.b4251e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2a371be86de05b6793",
+    "url": "/React-Portfolio-20/static/js/main.4c863695.chunk.js"
+  },
+  {
+    "revision": "372f97e06490b8c70c08",
+    "url": "/React-Portfolio-20/static/js/runtime-main.3ebbde81.js"
+  },
+  {
+    "revision": "d540bb9a885ca8b0220952134048b66e",
+    "url": "/React-Portfolio-20/static/media/0.d540bb9a.png"
+  },
+  {
+    "revision": "cf222c7be647ef676fef22c38cf25724",
+    "url": "/React-Portfolio-20/static/media/1.cf222c7b.png"
+  },
+  {
+    "revision": "3dd265e8013637027d9c674167ac33d4",
+    "url": "/React-Portfolio-20/static/media/2.3dd265e8.png"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/React-Portfolio-20/static/media/3.0d55b663.png"
+  },
+  {
+    "revision": "5ed4f11892a7d2c37d8b7b54b2a68da6",
+    "url": "/React-Portfolio-20/static/media/4.5ed4f118.png"
+  },
+  {
+    "revision": "3e339cb99042b642a3448d67a6e349eb",
+    "url": "/React-Portfolio-20/static/media/5.3e339cb9.png"
+  },
+  {
+    "revision": "b730820de0e43c2f5091a53a209f4388",
+    "url": "/React-Portfolio-20/static/media/backg.b730820d.jpg"
+  },
+  {
+    "revision": "08622d05dfaaf3baf0a6386c88b0c406",
+    "url": "/React-Portfolio-20/static/media/bill.08622d05.jpg"
+  },
+  {
+    "revision": "671d980cc4f3b5fcefd53214b7279305",
+    "url": "/React-Portfolio-20/static/media/resume.671d980c.txt"
+  }
+]);
