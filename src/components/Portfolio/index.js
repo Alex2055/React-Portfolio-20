fb10fbcdb453a5cd32d9cd097ea5b4dd3f6eccb3
Alex2055 <alexfromnashville@gmail.com>
2021-03-09 23:55:12 -0600

    projects layout and description

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,34 +3,40 @@ import React, { useState } from 'react'
 function Portfolio() {
   const [photos] = useState([
     {
-      name: 'Uncode Career',
+      name: 'UY Career',
       gitlink: 'https://github.com/cjacktwil/uncode-your-career',
-      applink: 'https://murmuring-everglades-03231.herokuapp.com'
+      applink: 'https://murmuring-everglades-03231.herokuapp.com',
+      description: 'DESCRIPTION: A MERN stack SPA that allows recent coding boot camp grads to search for available jobs in the industry.'
     },
     {
       name: 'Buy Smart',
       applink: 'https://fast-bastion-33726.herokuapp.com',
       gitlink: 'https://github.com/Alex2055/Buysmart',
+      description: 'sasdfasfdadaf'
     },
     {
       name: 'Doggyprdia',
       applink: 'https://kray306090.github.io/yorkie-terrier/',
       gitlink: 'https://github.com/KRAY306090/yorkie-terrier',
+      description: 'sasdfasfdadaf'
     },
     {
       name: 'Weather App',
       applink: 'https://alex2055.github.io/6th-week-challenge/index.html',
-      gitlink: 'https://github.com/Alex2055/third-week-challenge'
+      gitlink: 'https://github.com/Alex2055/third-week-challenge',
+      description: 'sasdfasfdadaf'
     },
     {
       name: 'Scheduler',
       gitlink: 'https://github.com/Alex2055/5th-week-challenge',
-      applink: 'https://alex2055.github.io/5th-week-challenge'
+      applink: 'https://alex2055.github.io/5th-week-challenge',
+      description: 'sasdfasfdadaf'
     },
     {
       name: 'Generator',
       gitlink: 'https://github.com/Alex2055/third-week-challenge',
       applink: 'https://alex2055.github.io/third-week-challenge',
+      description: 'sasdfasfdadaf'
     }
   ]);
 
@@ -47,6 +53,7 @@ function Portfolio() {
             </span>
             <div className="app-link"><a href={image.applink} className="project-name">{image.name}</a>
             </div>
+            <div className="desc">{image.description}</div>
           </div>
 
         ))}
